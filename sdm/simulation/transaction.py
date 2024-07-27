59bb58b2efafcf7e3a523293012627a184dc0e33
Mike Wang <wml816@gmail.com>
2024-07-26 23:04:34 -0400

    refactor transaction to use pydantic

--- sdm/simulation/transaction.py
+++ sdm/simulation/transaction.py
@@ -1,13 +1,14 @@
+from pydantic import BaseModel, field_validator, Field
+import datetime as dt
 from sdm.util.date_utils import datetime_to_string
 
 
-class Transaction:
-    def __init__(self, action, symbol, amount, price, datetime):
-        self.amount = amount
-        self.symbol = symbol
-        self.action = action
-        self.price = price
-        self.datetime = datetime
+class Transaction(BaseModel):
+    amount: int = Field(description="the number of shares to be traded in the transaction", gt=0)
+    symbol: str
+    action: int
+    price: float = Field(description="price of the stock in the transaction", gt=0)
+    datetime: dt.datetime
 
     def get_action_name(self):
         return "buy" if self.action > 0 else "sell" if self.action < 0 else "hold"
@@ -25,40 +26,9 @@ class Transaction:
                 "amount": self.amount,
                 "price": self.price}
 
-    @property
-    def amount(self):
-        return self._amount
-
-    @amount.setter
-    def amount(self, amount):
-        if not isinstance(amount, int) or amount < 0:
-            raise ValueError("Incorrect amount {} is assigned to a transaction!".format(amount))
-        self._amount = amount
-
-    @property
-    def symbol(self):
-        return self._symbol
-
-    @symbol.setter
-    def symbol(self, symbol):
-        self._symbol = symbol.upper()
-
-    @property
-    def action(self):
-        return self._action
-
-    @action.setter
-    def action(self, action):
+    @field_validator("action")
+    @classmethod
+    def action(cls, action):
         if action not in [-1, 0, 1]:
-            raise ValueError("action given {} is invalid. Must be one of these: -1, 0, 1".format(action))
-        self._action = action
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, price):
-        if price < 0:
-            raise ValueError("Incorrect price {} is assigned to a transaction!".format(price))
-        self._price = price
+            raise ValueError(f"action given {action} is invalid. Must be one of these: -1, 0, 1")
+        return action
